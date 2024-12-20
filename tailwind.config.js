3d50688c8740f75995c1d85ca245f18eb77b05be
theplantpsychologist@gmail.com <theplantpsychologist>
2024-12-20 11:22:13 -0800

    css updates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,js}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
